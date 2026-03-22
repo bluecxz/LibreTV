369731db690dd30916663db4fa62f05e9f720316
bluecxz <63791515+bluecxz@users.noreply.github.com>
2026-03-22 10:19:22 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,30 +3,7 @@ const CUSTOMER_SITES = {
         api: 'https://wolongzyw.com/api.php/provide/vod',
         name: 'WL',
     }
-    bf: {
-        api: 'https://bfzyapi.com/api.php/provide/vod',
-        name: 'bf',
-    }
-    js: {
-        api: 'https://jszyapi.com/api.php/provide/vod',
-        name: 'js',
-    }
-    wj: {
-        api: 'https://api.wujinapi.com/api.php/provide/vod',
-        name: 'wj',
-    }
-    ww: {
-        api: 'https://wwzy.tv/api.php/provide/vod',
-        name: 'ww',
-    }
-    gs: {
-        api: 'https://api.guangsuapi.com/api.php/provide/vod',
-        name: 'gs',
-    }
-    lz: {
-        api: 'https://cj.lziapi.com/api.php/provide/vod',
-        name: 'lz',
-    }
+    
 };
 
 // 调用全局方法合并
